561de85b9507f96dee27095fcb99845059abcf24
Pawel Karczewski <36538317+pablo1609@users.noreply.github.com>
2023-03-27 00:04:50 +0200

    Try Data annotations for handling empty strings in RegisterDto

--- ParkinApp.Domain/DTOs/RegisterDto.cs
+++ ParkinApp.Domain/DTOs/RegisterDto.cs
@@ -1,3 +1,13 @@
-namespace ParkinApp.Domain.DTOs;
+using System.ComponentModel.DataAnnotations;
 
-public record RegisterDto(string Username, string Password);
+namespace ParkinApp.Domain.DTOs
+{
+    public record RegisterDto
+    {
+        [Required(ErrorMessage = "Username cannot be empty.")]
+        public string Username { get; init; } = string.Empty;
+
+        [Required(ErrorMessage = "Password cannot be empty.")]
+        public string Password { get; init; } = string.Empty;
+    }
+}
