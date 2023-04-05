57bfddb2e449fa901918d50ce5023ab400c461a5
Remigiusz Zalewski <remigiuszzalewski@yahoo.com>
2023-04-05 17:18:28 +0200

    Added basic React structure with Login, Register and Routing.

---
+++ ParkinApp/Client/src/components/user/Login.js
@@ -0,0 +1,50 @@
+﻿import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import AuthService from "../../services/AuthService";
+
+const Login = () => {
+    const [userName, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+
+    const navigate = useNavigate();
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        try {
+            await AuthService.login(userName, password).then(
+                () => {
+                    navigate("/home");
+                    window.location.reload();
+                },
+                (error) => {
+                    console.log(error);
+                }
+            );
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    return (
+        <div>
+            <form onSubmit={handleLogin}>
+                <h3>Login</h3>
+                <input
+                    type="text"
+                    placeholder="user name"
+                    value={userName}
+                    onChange={(e) => setUserName(e.target.value)}
+                />
+                <input
+                    type="password"
+                    placeholder="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <button type="submit">Log in</button>
+            </form>
+        </div>
+    );
+};
+
+export default Login;
---
+++ ParkinApp/Client/src/services/AuthService.js
@@ -0,0 +1,49 @@
+﻿import axios from "axios";
+import API_ENDPOINT from "../config";
+
+const register = (username, password) => {
+    return axios
+        .post(API_ENDPOINT + "/register", {
+            username,
+            password,
+        })
+        .then((response) => {
+            if (response.data.accessToken) {
+                localStorage.setItem("user", JSON.stringify(response.data));
+            }
+
+            return response.data;
+        });
+};
+
+const login = (username, password) => {
+    return axios
+        .post(API_ENDPOINT + "/login", {
+            username,
+            password,
+        })
+        .then((response) => {
+            if (response.data.accessToken) {
+                localStorage.setItem("user", JSON.stringify(response.data));
+            }
+
+            return response.data;
+        });
+};
+
+const logout = () => {
+    localStorage.removeItem("user");
+};
+
+const getCurrentUser = () => {
+    return JSON.parse(localStorage.getItem("user"));
+};
+
+const AuthService = {
+    register,
+    login,
+    logout,
+    getCurrentUser,
+};
+
+export default AuthService;
